606bd04ad7a18dd7df633f415b3bfe4ca86b57a9
mragenks <theqar07@gmail.com>
2025-05-28 12:18:28 +0700

    bug home

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
                 </p>
 
                 <div className="flex justify-center space-x-4">
-                    <a href="#project" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">
+                    <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">
                         View Project
                     </a>
                     <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
